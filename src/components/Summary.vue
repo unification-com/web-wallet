e4b4a5316e70eaca99639891d4357e8c1a7c2bb8
Codegnosis <paul@codegnosis.co.uk>
2020-01-14 14:29:27 +0000

    info icons and popovers for wallet summary

--- src/components/Summary.vue
+++ src/components/Summary.vue
@@ -10,14 +10,22 @@
     </b-row>
 
     <b-row>
-      <b-col><h4 v-b-popover.hover.right="'Amount of UND available for transfer and staking'" title="Available Balance">Available Balance</h4></b-col>
+      <b-col>
+        <h4>
+          Available Balance <b-icon-info v-b-popover.hover.right="'Amount of UND available for transfer and staking'" title="Available Balance"/>
+        </h4>
+      </b-col>
       <b-col cols="9">
         <h4><span class="text-success">{{ w.balance }} UND</span></h4>
       </b-col>
     </b-row>
 
     <b-row v-show="w.staking.totalDelegations > 0">
-      <b-col><h4>Total Balance</h4></b-col>
+      <b-col>
+        <h4>
+          Total Balance <b-icon-info v-b-popover.hover.right="'Total UND in this wallet, including any staked UND, unbonding UND and Enterprise Locked UND'" title="Total Balance"/>
+        </h4>
+      </b-col>
       <b-col cols="9">
         <span class="text-info">{{ formatAmount(w.totalBalance) }}</span>
       </b-col>
@@ -25,7 +33,9 @@
 
     <b-row v-show="w.staking.totalDelegations > 0">
       <b-col>
-        <h4>Staking</h4>
+        <h4>
+          Staking <b-icon-info v-b-popover.hover.right="'Your current staking and delegations'" title="Staking"/>
+        </h4>
       </b-col>
       <b-col cols="9">
         <b-row>
@@ -38,24 +48,28 @@
         </b-row>
         <b-row>
           <b-col>Delegated</b-col>
-          <b-col>{{ formatAmount(w.staking.totalStaked) }}</b-col>
+          <b-col><span class="text-info">{{ formatAmount(w.staking.totalStaked) }}</span></b-col>
         </b-row>
         <b-row v-show="w.staking.totalUnbonding > 0">
           <b-col>Unbonding</b-col>
-          <b-col>{{ formatAmount(w.staking.totalUnbonding) }}</b-col>
+          <b-col><span class="text-info">{{ formatAmount(w.staking.totalUnbonding) }}</span></b-col>
         </b-row>
         <b-row>
           <b-col>Rewards</b-col>
-          <b-col>{{ formatAmount(w.staking.totalRewards) }}</b-col>
+          <b-col><span class="text-info">{{ formatAmount(w.staking.totalRewards) }}</span></b-col>
         </b-row>
       </b-col>
     </b-row>
 
     <b-row v-show="w.locked > 0">
       <b-col>
-        <h4>Enterprise Locked</h4>
+        <h4>
+          Enterprise Locked <b-icon-info v-b-popover.hover.right="'Amount of purchased locked Enterprise UND which can be used for WRKChain and BEACON fees'" title="Enterprise Locked"/>
+        </h4>
       </b-col>
-      <b-col cols="9">{{ w.locked }} UND</b-col>
+      <b-col cols="9">
+        <span class="text-info">{{ w.locked }} UND</span>
+      </b-col>
     </b-row>
   </b-container>
 </template>
