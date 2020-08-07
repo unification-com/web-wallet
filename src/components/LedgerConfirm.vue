45b2426eaf75793a66282333821bc1ab0e99c173
Codegnosis <paul@codegnosis.co.uk>
2020-08-07 16:52:39 +0100

    components - Ledger confirmation

---
+++ src/components/LedgerConfirm.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <p>
+      Please confirm and sign the transaction on your Ledger device.
+    </p>
+    <p><strong>Note</strong>: It may take a few seconds for the transaction to appear on your device.</p>
+  </div>
+</template>
