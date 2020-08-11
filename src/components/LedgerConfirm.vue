b5272d5618ca850d19730ae7b84c0311003bccf9
Codegnosis <paul@codegnosis.co.uk>
2020-08-11 13:28:30 +0100

    ledger devie notifications- add some styling

--- src/components/LedgerConfirm.vue
+++ src/components/LedgerConfirm.vue
@@ -1,8 +1,13 @@
 <template>
-  <div>
-    <p>
-      Please confirm and sign the transaction on your Ledger device.
-    </p>
-    <p><strong>Note</strong>: It may take a few seconds for the transaction to appear on your device.</p>
+  <div class="ledger-notification-wrapper">
+    <div class="ledger-notification">
+      <p>
+        Please confirm the transaction data above matches on your Ledger device. Sign the transaction on your
+        your Ledger device by clicking "Accept", or cancel the transaction by clicking "Reject".
+      </p>
+      <p>
+        <strong>Note</strong>: It may take a few seconds for the transaction data to appear on your device.
+      </p>
+    </div>
   </div>
 </template>
