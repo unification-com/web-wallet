15082be37fa73f4f0e6ff7d7d48a0f953137396f
jo <andreas.barthel@outlook.com>
2022-10-02 20:17:45 +0200

    Add a new "Recovery" tab for displaying the private key

---
+++ src/components/Recovery.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <b-modal id="bv-modal-show-private-key" size="lg">
+      <template v-slot:modal-title>
+        <h3>Your private key</h3>
+      </template>
+
+      <div>
+        <span class="private_key">{{ "0x" + client.privKey.toString("hex") }}</span>
+      </div>
+
+      <template v-slot:modal-footer>
+        <b-button variant="primary" @click="$bvModal.hide('bv-modal-show-private-key')">Close</b-button>
+      </template>
+    </b-modal>
+
+    <b-button variant="primary" @click="$bvModal.show('bv-modal-show-private-key')"
+      >Show private key</b-button
+    >
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex"
+
+export default {
+  name: "Recovery",
+  computed: {
+    ...mapState({
+      client: state => state.client.client,
+      chainId: state => state.client.chainId,
+      isClientConnected: state => state.client.isConnected,
+      wallet: state => state.wallet,
+      txs: state => state.txs,
+      validators: state => state.validators,
+      delegations: state => state.delegations,
+    }),
+  },
+}
+</script>
+
+<style scoped></style>
